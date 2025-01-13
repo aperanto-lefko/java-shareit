4b2e637acfce01d6dcedace316e46c6f7db9ba25
aperanto-lefko <polarster@yandex.ru>
2025-01-13 23:43:23 +0300

    Корректировка

--- server/src/test/java/ru/practicum/shareit/MockTests/strategy/StrategyCurrentTest.java
+++ server/src/test/java/ru/practicum/shareit/MockTests/strategy/StrategyCurrentTest.java
@@ -16,6 +16,7 @@ import ru.practicum.shareit.booking.stateStrategy.StrategyCurrent;
 import java.time.LocalDateTime;
 import java.util.List;
 
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
